ee1dd4e95c455b8c78da4fc252e05f68d8039964
choiyong1 <dragon02428@gmail.com>
2024-02-26 11:50:21 +0900

    news.csv

--- test.py
+++ test.py
@@ -1,8 +1,44 @@
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+import requests
+from datetime import date,timedelta
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
-url = ('https://news.naver.com/main/ranking/popularDay.naver?date=20240225')
-browser.get(url)
+now = date.today()
+# now_week = date.today() - timedelta(6)
+# start = now_week.strftime("%y%m%d")
+# stop = now.strftime("%y%m%d")
+start = now - timedelta(7)
+day = start
+
+# start_stop = range(int(start),int(stop)-1)
+company_name = ['YTN', 'KBS', 'SBS']
+df = pd.DataFrame(columns=company_name)
+
+# for day in range(int(start), int(stop)):
+for x in range(8):
+    day_string = day.strftime("%y%m%d")
+    url = f"https://news.naver.com/main/ranking/popularDay.naver?date=20{day_string}"
+    res = requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, 'lxml')
+    boxs = soup.find_all("div", attrs={"class": "rankingnews_box"})
+    for box in boxs:
+        news = []
+        name = box.find("strong", attrs={"class": "rankingnews_name"}).get_text()
+        if name in company_name:
+            titles = box.find_all("a", attrs={'class': "list_title nclicks('RBP.rnknws')"})
+            for i, title in enumerate(titles):
+                if i <= 2:
+                    news.append(f'{i + 1}번 :' + title.get_text())
+                else:
+                    pass
+            news = '\n'.join(news)
+            news.split('\n', )
+
+            df.loc[day_string, name] = news
+    day = day + timedelta(1)
+
+df.to_csv('news.csv', encoding='utf-8-sig')
+
+
+
--- webcrawling.py
+++ webcrawling.py
@@ -1,54 +1,32 @@
 import pandas as pd
-from selenium import webdriver
 from bs4 import BeautifulSoup
-import csv
 import requests
+import datetime
 
 company_name = ['YTN', 'KBS', 'SBS']
 
-# browser = webdriver.Chrome()
-df = pd.DataFrame(columns=company_name,  index=range(217,224))
+df = pd.DataFrame(columns=company_name,  index=range(17,24))
 
-for day in range(217, 224):
-    url = f"https://news.naver.com/main/ranking/popularDay.naver?date=20240{day}"
-    # browser.get(url)
-
+for day in range(17, 24):
+    url = f"https://news.naver.com/main/ranking/popularDay.naver?date=202402{day}"
     res = requests.get(url)
-
-    # page_source = browser.page_source
+    res.raise_for_status()
     soup = BeautifulSoup(res.text,'lxml')
-    boxs = soup.find_all("div",attrs={'class':'rankingnews_box'})
-
+    boxs = soup.find_all("div",attrs={"class": "rankingnews_box"})
     for box in boxs:
         news = []
-        name = soup.find("strong", attrs={'class': 'rankingnews_name'}).get_text()
+        name = box.find("strong",attrs={"class": "rankingnews_name"}).get_text()
         if name in company_name:
-            titles = soup.find_all("a", attrs={'class': "list_title nclicks('RBP.rnknws')"})
-            for title in titles:
-                news.append(title.get_text())
-            print(news)
+            titles = box.find_all("a", attrs={'class': "list_title nclicks('RBP.rnknws')"})
+            for i,title in enumerate(titles):
+                if i <= 2:
+                    news.append(f'{i+1}번 :' + title.get_text())
+                else:
+                    pass
+            news = '\n'.join(news)
+            news.split('\n',)
             df.loc[day, name] = news
 
-    # print(names)
-    # for name in names:
-    #     # print(name)
-    #     if name.get_text() == 'YTN':
-    #         print(name)
-    #         ytitles = soup.find_all("a", attrs={"class": "list_title nclicks('RBP.rnknws')"})
-    #         for i,ytitle in enumerate(ytitles):
-    #             df.loc[day, "YTN"] = (i,'번',ytitle.text)
-    #
-    #     elif name.get_text() == 'KBS':
-    #         print(name)
-    #         ktitles = soup.find_all("a", attrs={"class": "list_title nclicks('RBP.rnknws')"})
-    #         for i,ktitle in enumerate(ktitles):
-    #             df.loc[day, "KBS"] = (i,'번',ktitle.text)
-    #
-    #     elif name.get_text() == 'SBS':
-    #         print(name)
-    #         stitles = soup.find_all("a", attrs={"class": "list_title nclicks('RBP.rnknws')"})
-    #         for i,stitle in enumerate(stitles):
-    #             df.loc[day, "SBS"] = (i,'번',stitle.text)
 
 df.to_csv('news.csv',encoding='utf-8-sig')
 
